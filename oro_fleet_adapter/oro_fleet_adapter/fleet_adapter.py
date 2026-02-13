5e7b02d4d98ec7d2e973d93f9bfab4eca1660d31
santiago barragan <santiago.barragan@ekumenlabs.com>
2026-02-12 22:35:12 -0500

    refactor: change the reference coordinates

--- oro_fleet_adapter/oro_fleet_adapter/fleet_adapter.py
+++ oro_fleet_adapter/oro_fleet_adapter/fleet_adapter.py
@@ -114,9 +114,9 @@ def main(argv=sys.argv):
     fleet_config.server_uri = server_uri
 
     # Configure the transforms between robot and RMF frames
-    # for level, coords in config_yaml['reference_coordinates'].items():
-    #     tf = compute_transforms(level, coords, node)
-    #     fleet_config.add_robot_coordinates_transformation(level, tf)
+    for level, coords in config_yaml['reference_coordinates'].items():
+        tf = compute_transforms(level, coords, node)
+        fleet_config.add_robot_coordinates_transformation(level, tf)
 
     fleet_handle = adapter.add_easy_fleet(fleet_config)
 
